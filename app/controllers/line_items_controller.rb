a790b1dc7f4950ef0813b3da5ff073652c79f486
Catalin <catalin94v@gmail.com>
2015-05-21 18:48:27 +0100

    Added a delete button for the cart to delete items individually

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -56,9 +56,14 @@ class LineItemsController < ApplicationController
   # DELETE /line_items/1
   # DELETE /line_items/1.json
   def destroy
+    @line_item = LineItem.find(params[:id])
     @line_item.destroy
     respond_to do |format|
-      format.html { redirect_to line_items_url, notice: 'Line item was successfully destroyed.' }
+      if current_cart.line_items.empty?
+        format.html { redirect_to(root_path, :notice => 'Your cart is empty.')}
+      else
+        format.html { redirect_to(current_cart, :notice => 'Item Removed')}
+      end
       format.json { head :no_content }
     end
   end
